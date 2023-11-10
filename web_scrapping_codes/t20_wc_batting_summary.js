adfd234dc2034fcfbc0adbe880022411294517ab
Anurag Kumar <anuragkumardubey18@gmail.com>
2023-11-10 22:32:35 +0530

    Update t20_wc_batting_summary.js

--- web_scrapping_codes/t20_wc_batting_summary.js
+++ web_scrapping_codes/t20_wc_batting_summary.js
@@ -28,12 +28,10 @@ navigate(input.url);
 collect(parse());
 
 //------- 2.b Parser Code ------------//
-var match = $('div').filter(function(){
-	return $(this)
-      .find('span > span > span').text() === String("Match Details") 
-}).siblings()
-team1 = $(match.eq(0)).find('span > span > span').text().replace(" Innings", "")
-team2 = $(match.eq(1)).find('span > span > span').text().replace(" Innings", "")
+var matchDetails = $('div.ds-flex-row span.ds-inline-flex span.ds-text-tight-xs');
+
+var team1 = matchDetails.eq(0).text().replace(" Innings", "");
+var team2 = matchDetails.eq(1).text().replace(" Innings", "");
 matchInfo = team1 +  ' Vs ' + team2
 
 var tables = $('div > table.ci-scorecard-table');
